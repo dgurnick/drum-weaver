257afa526069d7a2f3a3a31303f9420f9b2ae978
Dennis Gurnick <dennis.gurnick@gmail.com>
2023-05-16 17:06:52 +0200

    Playing tests and included args plus volume of click

--- rust-edrum-waver/src/main.rs
+++ rust-edrum-waver/src/main.rs
@@ -1,28 +1,64 @@
 use std::fs::File;
 use std::io::BufReader;
-use rodio::{Decoder, OutputStream, Sink};
+use rodio::{Decoder, OutputStream};
+use std::thread;
+use clap::{Arg, ArgMatches};
 
 fn main() {
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&stream_handle).unwrap();
 
+    let matches = clap::Command::new("eDrums Wav Player")
+        .version("0.1")
+        .arg(Arg::new("track").required(true).index(1))
+        .arg(Arg::new("click").required(true).index(2))
+        .arg(Arg::new("click_volume").required(false).default_value("1").index(3))
+        .get_matches();
+
+    if let Err(err) = run(&matches) {
+        println!("Error: {}", err);
+        std::process::exit(1);
+    }
+
+}
+
+fn run(matches: &ArgMatches) -> Result<i32, String> {
+
+    let track_path_str = matches.get_one::<String>("track").expect("No track file provided");
+    let click_path_str = matches.get_one::<String>("click").expect("No click file provided");
+    let click_volume = matches.get_one::<String>("click_volume")
+        .unwrap_or(&"1.0".to_string())
+        .parse::<f32>()
+        .unwrap_or(1.0);
+
+    let track = File::open(track_path_str).map_err(|e| format!("Failed to open track file: {}", e))?;
+    let click = File::open(click_path_str).map_err(|e| format!("Failed to open click file: {}", e))?;
+
+    let (_track_stream, track_stream_handle) = OutputStream::try_default().unwrap();
     let (_click_stream, click_stream_handle) = OutputStream::try_default().unwrap();
-    let click_sink = Sink::try_new(&click_stream_handle).unwrap();
+    
 
-    let (_stream, _) = OutputStream::try_default().unwrap();
-    let file = BufReader::new(File::open("examples/test.wav").unwrap());
+    let track_sink = rodio::Sink::try_new(&track_stream_handle).map_err(|e| format!("Failed to create audio sink: {}", e))?;
+    let click_sink = rodio::Sink::try_new(&click_stream_handle).map_err(|e| format!("Failed to create audio sink: {}", e))?;
+    click_sink.set_volume(click_volume);
 
-    let (_stream_click, _) = OutputStream::try_default().unwrap();
-    let click_file = BufReader::new(File::open("examples/test_click.wav").unwrap());
+    let track_source = Decoder::new(BufReader::new(track)).map_err(|e| format!("Failed to decode track file: {}", e))?;
+    let click_source = Decoder::new(BufReader::new(click)).map_err(|e| format!("Failed to decode click file: {}", e))?;
 
-    let source = Decoder::new(file).unwrap();
-    let click_source = Decoder::new(click_file).unwrap();
+    let track_thread = thread::spawn(move || {
+        //track_stream_handle.play_raw(track_source.convert_samples()).unwrap();
+        track_sink.append(track_source);
+        track_sink.sleep_until_end();
+        
+    });
 
-    sink.append(source);
-    click_sink.append(click_source);
+    let click_thread = thread::spawn(move || {
+        //click_stream_handle.play_raw(click_source.convert_samples()).unwrap();
+        click_sink.append(click_source);
+        click_sink.sleep_until_end();
+    });
 
-    // The sound plays in a separate thread. This call will block the current thread until the sink
-    // has finished playing all its queued sounds.
-    click_sink.play();
-    sink.sleep_until_end();
+    track_thread.join().expect("track thread panicked");
+    click_thread.join().expect("click thread panicked");
+
+    Ok(0)
+
 }
