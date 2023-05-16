642668e8aef1c4edcbb7f98208bead9cdaea6a85
Dennis Gurnick <dennis.gurnick@gmail.com>
2023-05-16 22:08:32 +0200

    Adding combined as an option to figure out the threading. Also added proper arge names ftw

--- rust-edrum-waver/src/main.rs
+++ rust-edrum-waver/src/main.rs
@@ -1,8 +1,9 @@
 use std::fs::{metadata, File};
-use std::io::BufReader;
+use std::io::{BufReader, BufRead};
 use std::path::PathBuf;
 use std::time::Duration;
 use rodio::*;
+use rodio::source::Amplify;
 use rodio::cpal::traits::{HostTrait};
 use std::{thread, println};
 use clap::{Arg, ArgMatches};
@@ -29,11 +30,13 @@ fn main() {
 
     let matches = clap::Command::new("eDrums Wav Player")
         .version("0.1")
-        .arg(Arg::new("music_folder").required(true).index(1).help("Where your music files are stored"))
-        .arg(Arg::new("track").required(true).index(2).help("Position in the csv file to play"))
-        .arg(Arg::new("track_volume").required(false).default_value("100").index(3))
-        .arg(Arg::new("click_volume").required(false).default_value("100").index(4))
-        .arg(Arg::new("track_device").required(false).default_value("1").index(5))
+        .arg(Arg::new("music_folder").long("music_folder").required(true).help("Where your music files are stored"))
+        .arg(Arg::new("track").long("track").required(true).help("Position in the csv file to play"))
+        .arg(Arg::new("track_volume").long("track_volume").required(false).default_value("100"))
+        .arg(Arg::new("click_volume").long("click_volume").required(false).default_value("100"))
+        .arg(Arg::new("track_device").long("track_device").required(false).default_value("1"))
+        .arg(Arg::new("click_device").long("click_device").required(false).default_value("1"))
+        .arg(Arg::new("combined").long("combined").required(false).default_value("1"))
         .get_matches();
 
     if let Err(err) = run(&matches) {
@@ -94,7 +97,7 @@ fn get_file_paths(music_folder: &str, song_position: usize) -> Result<(String, S
                 // if there's a 7z file with the same name, decompress it 
                  let archive_path = PathBuf::from(format!("{}/{}/{}.7z", music_folder, song.folder, song.file_name));
                  if ! archive_path.exists() {
-                    return Err("Failed to find file or 7z archive".to_string());
+                    return Err(format!("Failed to find file or 7z archive for {}", archive_path.display()));
                  } 
                  println!("Decompressing file: {}", archive_path.display());
 
@@ -188,6 +191,14 @@ fn run(matches: &ArgMatches) -> Result<i32, String> {
         .unwrap_or(&"1".to_string())
         .parse::<usize>()
         .unwrap_or(1) - 1;
+    let click_device_position = matches.get_one::<String>("click_device")
+        .unwrap_or(&"1".to_string())
+        .parse::<usize>()
+        .unwrap_or(1) - 1;
+    let combined = matches.get_one::<String>("combined")
+        .unwrap_or(&"1".to_string())
+        .parse::<usize>()
+        .unwrap_or(1);
 
     let host = cpal::default_host();
     let available_devices = host.output_devices().unwrap().collect::<Vec<_>>();
@@ -198,7 +209,10 @@ fn run(matches: &ArgMatches) -> Result<i32, String> {
     // Check if the device positions are valid
     let num_devices = available_devices.len();
     if track_device_position > num_devices {
-        return Err("Invalid device position".to_string());
+        return Err("Invalid track output device".to_string());
+    }
+    if click_device_position > num_devices {
+        return Err("Invalid click output device".to_string());
     }
 
     let track = File::open(track_path_str).map_err(|e| format!("Failed to open track file: {}", e))?;
@@ -209,13 +223,70 @@ fn run(matches: &ArgMatches) -> Result<i32, String> {
     let track_source_amplify = track_source.amplify(track_volume);
     let click_source_amplify = click_source.amplify(click_volume);
 
-    let (_stream, stream_handle) = OutputStream::try_from_device(&available_devices[track_device_position]).map_err(|e| format!("Failed to create track output stream: {}", e))?;
-    let combined_source = track_source_amplify.mix(click_source_amplify);
+    if combined == 1 {
+        match play_combined(
+            track_source_amplify, 
+            click_source_amplify, 
+            &available_devices[track_device_position]
+        ) {
+            Ok(_) => {},
+            Err(err) => return Err(err),
+        }
+    
+    } else {
+        match play_separate(
+            track_source_amplify, 
+            click_source_amplify, 
+            &available_devices[track_device_position],
+            &available_devices[click_device_position],
+            track_volume,
+            click_volume
+        ) {
+            Ok(_) => {},
+            Err(err) => return Err(err),
+
+        }
+    }
+
+    Ok(0)
+
+}
+
+fn play_combined(track_source: Amplify<Decoder<BufReader<File>>>, click_source: Amplify<Decoder<BufReader<File>>>, device: &Device) -> Result<(), String> {
+    let (_stream, stream_handle) = OutputStream::try_from_device(&device).map_err(|e| format!("Failed to create track output stream: {}", e))?;
+    let combined_source = track_source.mix(click_source);
 
     let sink = Sink::try_new(&stream_handle).unwrap();
     sink.append(combined_source);
     sink.sleep_until_end();
+    Ok(())
+}
 
-    Ok(0)
+fn play_separate(track_source: Amplify<Decoder<BufReader<File>>>, click_source: Amplify<Decoder<BufReader<File>>>, track_device: &Device, click_device: &Device, track_volume: f32, click_volume: f32) -> Result<(), String> {
+    
+    let (_track_stream, track_stream_handle) = OutputStream::try_from_device(track_device).map_err(|e| format!("Failed to create track output stream: {}", e))?;
+    let (_click_stream, click_stream_handle) = OutputStream::try_from_device(click_device).map_err(|e| format!("Failed to create track output stream: {}", e))?;
 
+    let track_sink = rodio::Sink::try_new(&track_stream_handle).map_err(|e| format!("Failed to create audio sink: {}", e))?;
+    let click_sink = rodio::Sink::try_new(&click_stream_handle).map_err(|e| format!("Failed to create audio sink: {}", e))?;
+    track_sink.set_volume(track_volume);
+    click_sink.set_volume(click_volume);
+
+    track_sink.append(track_source);
+    click_sink.append(click_source);
+
+    let track_thread = thread::spawn(move || {
+        track_sink.sleep_until_end();
+    });
+
+    let click_thread = thread::spawn(move || {
+        click_sink.sleep_until_end();
+
+    }); 
+    
+
+    track_thread.join().expect("track thread panicked");
+    click_thread.join().expect("click thread panicked");
+
+    Ok(())
 }
