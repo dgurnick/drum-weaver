d73b553b6495288344b61c3cd1c91e1f4cba3b6b
Dennis Gurnick <dennis.gurnick@gmail.com>
2023-05-17 22:57:25 +0200

    Refactored. But fails. yolo

--- rust-edrum-waver/src/main.rs
+++ rust-edrum-waver/src/main.rs
@@ -1,49 +1,14 @@
-use chrono::prelude::*;
-use crossterm::{
-    event::{self, Event as CEvent, KeyCode, KeyEvent, KeyEventKind},
-    terminal::{disable_raw_mode, enable_raw_mode},
-};
-use std::{
-    fs,
-    io,
-    path::PathBuf,
-    sync::{mpsc, Mutex},
-    thread,
-    time::{Duration, Instant},
-};
-
 use clap::{Arg, ArgMatches};
-use csv;
-use rodio::{
-    self, 
-    cpal, 
-    cpal::traits::HostTrait,
-    Decoder,
-    Source,
-};
-use serde::{Deserialize, Serialize};
-use thiserror::Error;
-use tui::{
-    backend::CrosstermBackend,
-    layout::{Alignment, Constraint, Direction, Layout},
-    style::{Color, Modifier, Style},
-    text::{Span, Spans},
-    widgets::{Block, BorderType, Borders, Cell, List, ListItem, ListState, Paragraph, Row, Table, Tabs},
-    Terminal,
-};
-use lazy_static::lazy_static;
-
-mod common;
-use common::Song;
 
 mod player;
-use player::play_combined;
-use player::play_separate;
+use player::run_cli;
 
-mod util;
-use util::get_file_paths;
+mod common;
+use common::get_file_paths;
+use common::PlayerArguments;
 
-
+mod ui;
+use ui::run_ui;
 
 fn main() {
 
@@ -65,10 +30,6 @@ fn main() {
     }
 
 }
-
-
-
-
 
 /// Runs the program as determined by the main function
 fn run(matches: &ArgMatches) -> Result<i32, String> {
@@ -114,116 +75,21 @@ fn run(matches: &ArgMatches) -> Result<i32, String> {
         .map(|value| value == "1")
         .unwrap_or(false);
 
+    let arguments = PlayerArguments {  
+        music_folder: music_folder.to_string(),
+        track_volume: track_volume,
+        click_volume: click_volume,
+        track_device_position: track_device_position,
+        click_device_position: click_device_position,
+        combined: combined, 
+    };
+
     if ui {
-        setupUi(); 
+        run_ui(arguments); 
     } else {
-        let host = cpal::default_host();
-        let available_devices = host.output_devices().unwrap().collect::<Vec<_>>();
-        if available_devices.is_empty() {
-            return Err("No output devices found".to_string());
-        }
-
-        // Check if the device positions are valid
-        let num_devices = available_devices.len();
-        if track_device_position > num_devices {
-            return Err("Invalid track output device".to_string());
-        }
-        if click_device_position > num_devices {
-            return Err("Invalid click output device".to_string());
-        }
-
-        let track = fs::File::open(track_path_str).map_err(|e| format!("Failed to open track file: {}", e))?;
-        let click = fs::File::open(click_path_str).map_err(|e| format!("Failed to open click file: {}", e))?;
-
-        let track_source = Decoder::new(io::BufReader::new(track)).map_err(|e| format!("Failed to decode track file: {}", e))?;
-        let click_source = Decoder::new(io::BufReader::new(click)).map_err(|e| format!("Failed to decode click file: {}", e))?;
-        let track_source_amplify = track_source.amplify(track_volume);
-        let click_source_amplify = click_source.amplify(click_volume);
-
-        if combined {
-            match play_combined(
-                track_source_amplify, 
-                click_source_amplify, 
-                &available_devices[track_device_position]
-            ) {
-                Ok(_) => {},
-                Err(err) => return Err(err),
-            }
-        
-        } else {
-            match play_separate(
-                track_source_amplify, 
-                click_source_amplify, 
-                &available_devices[track_device_position],
-                &available_devices[click_device_position],
-                track_volume,
-                click_volume
-            ) {
-                Ok(_) => {},
-                Err(err) => return Err(err),
-
-            }
-        }
-
-    }
+        run_cli(arguments);
+   }
 
     Ok(0)
 
 }
-
-
-
-
-
-
-
-
-
-
-#[derive(Serialize, Deserialize, Clone)]
-struct Playlist {
-    id: usize,
-    name: String,
-    category: String,
-    age: usize,
-    created_at: DateTime<Utc>,
-}
-
-
-enum Event<I> {
-    Input(I),
-    Tick,
-}
-
-#[derive(Error, Debug)]
-pub enum Error {
-    #[error("error reading the DB file: {0}")]
-    ReadDBError(#[from] io::Error),
-    #[error("error parsing the DB file: {0}")]
-    ParseDBError(#[from] serde_json::Error),
-    #[error("error parsing the CsV file: {0}")]
-    CsvError(#[from] csv::Error),
-}
-
-#[derive(Copy, Clone, Debug)]
-enum MenuItem {
-    Home,
-    Playlists,
-    Songs,
-}
-
-impl From<MenuItem> for usize {
-    fn from(input: MenuItem) -> usize {
-        match input {
-            MenuItem::Home => 0,
-            MenuItem::Playlists => 1,
-            MenuItem::Songs => 2,
-        }
-    }
-}
-
-// see: https://github.com/zupzup/rust-commandline-example/blob/main/src/main.rs
-
-       
-
-
