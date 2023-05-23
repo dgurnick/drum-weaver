6fe503aee2fbbc854896daae8d539acbf9c9982a
Dennis Gurnick <dennis.gurnick@gmail.com>
2023-05-23 22:03:50 +0200

    Missed a file

---
+++ rust-edrum-waver/src/app/components/player_component.rs
@@ -0,0 +1,28 @@
+use super::AppComponent;
+use crate::app::App;
+use std::sync::atomic::Ordering::Relaxed;
+
+pub struct PlayerComponent;
+
+impl AppComponent for PlayerComponent {
+    type Context = App;
+
+    fn add(ctx: &mut Self::Context, ui: &mut eframe::egui::Ui) {
+        ui.horizontal(|ui| {
+            let stop_btn = ui.button("■");
+            let play_btn = ui.button("▶");
+            let pause_btn = ui.button("⏸");
+            let prev_btn = ui.button("|◀");
+            let next_btn = ui.button("▶|");
+
+            let mut volume = ctx.player.as_ref().unwrap().volume;
+            ui.add(
+                eframe::egui::Slider::new(&mut volume, (0.0 as f32)..=(5.0 as f32))
+                    .logarithmic(false)
+                    .show_value(false)
+                    .clamp_to_range(true),
+            );
+            ctx.player.as_mut().unwrap().set_volume(volume);
+        });
+    }
+}
